b28e3a30e1c900b353f6f1360fc50dd68ecfdcab
Wildan <wildanwigenta52@gmail.com>
2025-05-29 21:51:45 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,21 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
+Route::get('/filament', function () {
+    return view('filament');
+})->middleware(['auth'])->name('filament');
+Route::get('/filament/resources', function () {
+    return view('filament.resources');
+})->middleware(['auth'])->name('filament.resources');
+Route::get('/filament/resources/users', function () {
+    return view('filament.resources.users');
+})->middleware(['auth'])->name('filament.resources.users');
+Route::get('/filament/resources/posts', function () {
+    return view('filament.resources.posts');
+})->middleware(['auth'])->name('filament.resources.posts');
+Route::get('/filament/resources/comments', function () {
+    return view('filament.resources.comments');
+})->middleware(['auth'])->name('filament.resources.comments');
+Route::get('/filament/resources/settings', function () {
+    return view('filament.resources.settings');
+})->middleware(['auth'])->name('filament.resources.settings');
