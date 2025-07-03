56e3efa3487733ae5dcc0b8ae32b321b1e378e11
Wildan <wildanwigenta52@gmail.com>
2025-07-03 23:47:36 +0700

    update pest

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,7 +11,11 @@
 |
 */
 
+pest()->extend(Tests\TestCase::class)
+ // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature');
 
+/*
 |--------------------------------------------------------------------------
 | Expectations
 |--------------------------------------------------------------------------
